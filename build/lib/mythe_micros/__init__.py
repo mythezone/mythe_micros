0bf7db43668e4a438c53b0023afb337b31d5de01
mythezone <125616515@qq.com>
2019-12-23 11:27:04 +0800

    updated to 0.0.1

--- build/lib/mythe_micros/__init__.py
+++ build/lib/mythe_micros/__init__.py
@@ -1,2 +1,3 @@
 #-*- coding:utf-8 -*-
 from .mytest import mysum,output
+from .utils import cprint,cprints
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name= "mythe_micros",
-    version = "0.0.0",
+    version = "0.0.1",
     keywords=("pip","micro","service"),
     description=("Easy Micro Service Library"),
     long_description=("To be fill."),
